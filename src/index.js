ae07048644864df822eb207d97f81685493b2494
Vladimir Zaburianov <zaburianov@gmail.com>
2020-10-01 18:02:22 +0300

    init: start typical-arrays-problems task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
 
 exports.min = function min (array) {
-  return 0;
+    if (!array || array.length === 0){
+        return 0;
+    } else
+    {return array.reduce((minNumber, item) => {
+      if (item < minNumber) {
+          return item;
+      } else return minNumber;
+  }, Infinity);}
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (!array || array.length === 0){
+        return 0;
+    } else {
+    return array.reduce((maxNumber, item) => {
+    if (item > maxNumber) {
+        return item;
+    } else return maxNumber;
+}, -Infinity);}
+
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (!array || array.length === 0){
+        return 0;
+    } else {
+        let average = array.reduce((sum, item) => sum + item, 0);
+        return (average / array.length);
+    }
 }
